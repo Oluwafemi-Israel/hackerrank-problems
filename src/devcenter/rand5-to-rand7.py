b6b32821969134b80f07669a643a05c7f6c0dce7
Oluwafemi Shobande <oluwafemi.shobande@interswitchgroup.com>
2019-05-22 11:37:32 +0100

    explanation

--- src/devcenter/rand5-to-rand7.py
+++ src/devcenter/rand5-to-rand7.py
@@ -12,6 +12,16 @@ def generate_bit():
     return num & 1
 
 
+# This may not be the most efficient, but it works
+#
+# 7 can be represented by 3 bits.
+#
+# If we employ rand5() in generating 1s or 0s with equal probability, we can populate the 3 required bits.
+#
+# The question of how to generate 1s or 0s remains. One idea is to use the oddness or evenness of rand5()
+# to select the bit `e.g odd gives 1, even gives 0`.
+# However, there are 3 possible odds, and 2 possible evens for rand5().
+# To level the playing ground we pick any of the odds and reject it always (e.g 5)
 def rand7():
     sum_ = 0
     while sum_ == 0:
