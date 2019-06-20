683b7356b023471e1291f45df89afda1983a8679
Oluwafemi Shobande <oluwafemi.shobande@interswitchgroup.com>
2019-06-20 10:05:20 +0100

    super efficient exponentiation

---
+++ src/devcenter/pow.py
@@ -0,0 +1,59 @@
+# Implement integer exponentiation. That is, implement the pow(x, y) function, where x and y are integers and returns
+#  x^y.
+#
+# Do this faster than the naive method of repeated multiplication.
+
+
+def binary(num):
+    result = ""
+    quotient = num
+    while quotient != 0:
+        remainder = quotient % 2
+        quotient = quotient // 2
+        result = "{0}{1}".format(remainder, result)
+
+    return result
+
+
+def reduce_exponent(exponent):
+    exponent_in_binary = binary(exponent)
+    num_of_digits = len(exponent_in_binary)
+
+    reduced_exponents = []
+
+    for i in range(num_of_digits):
+        if exponent_in_binary[num_of_digits - i - 1] == "1":
+            reduced_exponents.append(2 ** i)
+
+    return reduced_exponents
+
+
+def populate_memory(base, greatest_exponent):
+    memory = {1: base}
+    current_exponent = 2
+    while current_exponent <= greatest_exponent:
+        memory[current_exponent] = memory[current_exponent // 2] ** 2
+        current_exponent = current_exponent * 2
+
+    return memory
+
+
+def power(base, exponent):
+    if exponent == 0:
+        return 1
+    if exponent == 1:
+        return base
+
+    reduced_exponent = reduce_exponent(exponent)
+
+    memory = populate_memory(base, reduced_exponent[-1])
+
+    result = 1
+    for exponent in reduced_exponent:
+        result = result * memory[exponent]
+
+    return result
+
+
+if __name__ == "__main__":
+    print(power(5, 0))
